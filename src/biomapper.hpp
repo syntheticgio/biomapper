b1413f99d7eee5aec32c016b2e186a503561f45e
jackfrost2199 <jackfrost2199@gmail.com>
2015-07-18 20:40:04 -0400

    Continued writing program

--- src/biomapper.hpp
+++ src/biomapper.hpp
@@ -5,6 +5,13 @@
 #define __BIOMAPPER_HPP_INCLUDED__
 
 enum CrossMapType { OVERLAP, EXCLUSIVE };
+class idx
+{
+public:
+  idx () : val(0);
+  int val;
+};
+
 
 class BioMapper
 {
@@ -13,18 +20,40 @@ class BioMapper
 		
 	  BioMapper();
 
-	  void determineReferences();
+	  bool determineReferences();
 	  bool determineArguments(int argc, char** argv);
+	  
+	  bool argumentCleanup ();
+	  
 
 	  // Debugging
 	  void printFiles ();
 	  int returnNumberOfAnnotationFiles ();
+	  void printClassVariables ();
 	
 	private:
 	  CrossMapType mappingStyle;
+	  int chromosomeColumn;
+	  int startColumn;
+	  int endColumn;
+	  int lastColumn;
+	  bool header;
+	  std::string fileType;
 	  std::vector <std::string> annotationFiles;
-	  std::vector <std::string> referenceNames;
-
+	  std::vector <std::string> headerRows;
+	  std::map <std::string, idx> referenceIDs;
+	  
+	  bool setChromosomeColumn (int column);
+	  bool setStartColumn (int column);
+	  bool setEndColumn (int column);
+	  bool setFileType (const std::string file_type);
+	  bool setHeader (bool hdr);
+	  
+	  // Cleanup
+	  bool verifyEndColumn();
+	  bool setLastColumn();
 };
+
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,26 @@ int main (int argc, char* argv[])
 	BioMapper myMap;
 	bool validateArguments = myMap.determineArguments(argc, argv);
 	if (!validateArguments) {
-	    cout << "Invalid arguments used \n\n";
-	    cerr << "Invalid Arguments\n\n";
+	    cout << "Invalid arguments used. See error file for more information." << endl << endl;
+	    cerr << "ERROR: Invalid Arguments used." << endl << endl;
+	    return 0;
 	}
+	
+	// Verify end column number and set if needed (since this may or may not be passed in as an argument)
+	// Also set the last column value to speed up reference id reading.
+	bool argumentCleanup = myMap.argumentCleanup();
+	
+	if (!argumentCleanup) {
+	 cout << "Invalid arguments cleanup. See error file for more information." << endl << endl;
+	 cerr << "ERROR: Invalid arguments cleanup." << endl << endl;
+	 return 0;
+	}
+	
+	// DEBUG
 	myMap.printFiles();
 	
 	// Currently have files from command line
+	// also have start, end, and reference (chromosome) id columns.
 	// Need to determine references and make sure they match
 	// Include 'smart' reference matching flag?
 	
